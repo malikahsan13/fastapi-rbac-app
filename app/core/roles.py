93dc3f5808ab6573d5b81b66ce0df2190d36c912
Malik Ahsan <malikahsan13@gmail.com>
2025-06-12 19:19:58 +0500

    added logic for checking roles

--- app/core/roles.py
+++ app/core/roles.py
@@ -5,4 +5,10 @@ from app.core.jwt_handler import decode_token
 security = HTTPBearer()
 
 def require_role(required_roles: list):
-    
+    def wrapper(credentials: HTTPAuthorizationCredentials = Depends(security)):
+        token = credentials.credentials
+        payload = decode_token(token)
+        if not payload or payload.get("role") not in required_roles:
+            raise HTTPException(403, detail="Insufficient role privileges")
+        return payload
+    return wrapper
